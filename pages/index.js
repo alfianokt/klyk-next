87db25f99527be0585e82c7b85156fde4c1f222f
Alfian Oktafireza <alfianokt104@gmail.com>
2022-05-19 22:33:17 +0700

    add windicss

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="bg-blue-500">Hello</h1>
-    </div>
-  )
-}
---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-[100vh] items-center justify-center">
+      <button className="bg-gray-200 text-blue-500 p-3 rounded hover:bg-gray-300 transition-colors">Hello</button>
+    </div>
+  )
+}
