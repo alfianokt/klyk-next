28e4a4f2376b595ce2dc0eb3e9acb5b3c98d7e30
Alfian Oktafireza <alfianokt104@gmail.com>
2022-05-23 11:11:00 +0700

    add colors and form

--- windi.config.js
+++ windi.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'windicss/helpers'
+import forms from 'windicss/plugin/forms'
 
 export default defineConfig({
   extract: {
     include: ['**/*.{jsx,tsx,css}'],
     exclude: ['node_modules', '.git', '.next'],
   },
+  plugins: [
+    forms
+  ],
   theme: {
     extend: {
       colors: {
         brand: {
           purple: '#6D4BEA',
           grey: '#A6A6A6',
+          black: '#0A0A0A',
           dark: '#E5E6EB',
           light: '#FAFAFE',
           secondary: '#F0EDFD',
