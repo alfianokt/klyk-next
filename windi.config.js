ca01aaeb4f2ce943d7479accdd8b434ebcbbfe88
Alfian Oktafireza <alfianokt104@gmail.com>
2022-05-23 10:49:23 +0700

    init theme

--- windi.config.js
+++ windi.config.js
@@ -5,4 +5,20 @@ export default defineConfig({
     include: ['**/*.{jsx,tsx,css}'],
     exclude: ['node_modules', '.git', '.next'],
   },
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          purple: '#6D4BEA',
+          grey: '#A6A6A6',
+          dark: '#E5E6EB',
+          light: '#FAFAFE',
+          secondary: '#F0EDFD',
+        }
+      },
+      fontFamily: {
+        body: ['Work Sans', 'sans-serif']
+      }
+    }
+  }
 })
