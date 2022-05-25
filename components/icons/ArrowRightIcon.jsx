af65068df00dfb6bb4190a9d188c7ab33e7a68aa
Alfian Oktafireza <alfianokt104@gmail.com>
2022-05-25 10:25:41 +0700

    add icos

---
+++ components/icons/ArrowRightIcon.jsx
@@ -0,0 +1,10 @@
+export default function ArrowRightIcon() {
+  return (
+    <>
+      <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5.42893 12H19.5711" stroke="white" strokeWidth="2" />
+        <path d="M12.5 4.92893L19.5711 12L12.5 19.0711" stroke="white" strokeWidth="2" />
+      </svg>
+    </>
+  )
+}
