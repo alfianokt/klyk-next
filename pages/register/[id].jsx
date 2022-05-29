d774381c84ea16b365b28e6b918b7325858e1d5f
Alfian Oktafireza <alfianokt104@gmail.com>
2022-05-29 12:53:45 +0700

    finish register page

--- pages/register/[id].jsx
+++ pages/register/[id].jsx
@@ -1,12 +1,59 @@
-import { useRouter } from "next/router"
+import { models } from "mongoose";
+import { connect } from "../../models";
 
-export default function Index() {
-  const router = useRouter();
-  const { id } = router.query;
+const { BONUS_POINT = 10 } = process.env;
 
-  return (
-    <>
-      <h1>Register page : {id}</h1>
-    </>
-  )
+export async function getServerSideProps(context) {
+  await connect();
+  const { id: referal_id } = context.query;
+
+  // get referal data
+  const data = await models.ListReferalLink
+    .findOne({
+      referal_id,
+    });
+
+  // referal data not found
+  if (!data) return { notFound: true }
+
+  // get invitation data
+  const invitation_data = await models.InvitationList
+    .findOne({
+      referalId: data._id,
+    });
+
+  // invitation data not found
+  if (!invitation_data) return { notFound: true }
+
+  // update status invitation if not readed
+  if (invitation_data.status_invitation == 0) {
+    // add point to user who invite
+    await models.User
+      .updateOne({
+        _id: data.userId,
+      }, {
+        $inc: {
+          total_point: BONUS_POINT,
+        }
+      });
+
+    // set invitation status to read
+    await models.InvitationList
+      .updateOne({
+        referalId: data._id,
+      }, {
+        status_invitation: 1,
+      });
+  }
+
+  return {
+    redirect: {
+      destination: 'https://ourklyk.com/signup',
+      permanent: false,
+    },
+  }
+}
+
+export default function Register() {
+  return <></>;
 }
